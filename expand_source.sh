71b605ce28d2d4fe9de0fb196e4e8354ab49602d
rockerbacon <vitor@librem.one>
2020-06-14 17:11:37 -0300

    Fixed script minifying EOF strings

--- expand_source.sh
+++ expand_source.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 declare -A already_expanded
 
 function expand_source {
@@ -5,7 +7,8 @@ function expand_source {
 		print_error "Bashminify error in \"expand_source\", nr. of arguments"
 	fi
 
-	local filename=$(realpath "$1")
+	local filename
+	filename=$(realpath "$1")
 	if [[ ! -f "${filename}" ]]; then
 		print_error "File \"${filename}\" does not exist."
 		exit 1
@@ -16,12 +19,13 @@ function expand_source {
 		exit 1
 	fi
 
-	local base_source_dir=$(dirname "$filename")
+	local base_source_dir
+	base_source_dir=$(dirname "$filename")
 	while read -r line; do
-		command="$(echo $line | cut -d" " -f1)"
+		command="$(echo "$line" | cut -d" " -f1)"
 
 		if [[ "$command" == "source" || "$command" == "." ]]; then
-			include_file="$base_source_dir/$(echo $line | cut -d" " -f 2)"
+			include_file="$base_source_dir/$(echo "$line" | cut -d" " -f 2)"
 
 			if [[ "${already_expanded[$include_file]}" == "" ]]; then
 				already_expanded[$include_file]=true
