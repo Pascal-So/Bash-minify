efc7a479e05eec34bc32c5cb0e77b9189a40e9f8
rockerbacon <vitor@librem.one>
2020-06-14 13:15:59 -0300

    Retabbed to fix inconsistencies

--- bashminify.sh
+++ bashminify.sh
@@ -8,7 +8,7 @@
 set -u
 
 function print_error {
-    echo -e "\033[0;31mERROR\033[0m:" $@ >&2
+	echo -e "\033[0;31mERROR\033[0m:" $@ >&2
 }
 
 source read_args.sh
@@ -16,13 +16,13 @@ source read_args.sh
 source expand_source.sh
 
 function strip_comments_and_whitespace {
-    sed -e '/^\s*$/d' -e '/^#[^!].*$/d'
+	sed -e '/^\s*$/d' -e '/^#[^!].*$/d'
 }
 
 function use_minimal_function_header {
-    sed -E 's/function ([a-zA-Z0-9_]*) \{/\1 () {/'
+	sed -E 's/function ([a-zA-Z0-9_]*) \{/\1 () {/'
 }
 
 expand_source "${input_file}" |
-    strip_comments_and_whitespace |
-    use_minimal_function_header
+	strip_comments_and_whitespace |
+	use_minimal_function_header
--- expand_source.sh
+++ expand_source.sh
@@ -1,26 +1,26 @@
 function expand_source {
-    if [[ $# -ne 1 ]]; then
-	print_error "Bashminify error in \"expand_source\", nr. of arguments"
-    fi
-    
-    local filename=$1
-    if [[ ! -f "${filename}" ]]; then
-	print_error "File \"${input_file}\" does not exist."
-	exit 1
-    fi
-
-    if [[ ! -r "${filename}" ]]; then
-	print_error "Can't read file \"${input_file}\"."
-	exit 1
-    fi
-
-    while read -r line; do
-	command="$(echo $line | cut -d" " -f1)"
-	if [[ "$command" == "source" || "$command" == "." ]]; then
-	    included_file=$(echo $line | cut -d" " -f 2)
-	    expand_source "${included_file}"
-	else
-	    echo "$line"
+	if [[ $# -ne 1 ]]; then
+		print_error "Bashminify error in \"expand_source\", nr. of arguments"
 	fi
-    done <"${filename}"
+
+	local filename=$1
+	if [[ ! -f "${filename}" ]]; then
+		print_error "File \"${input_file}\" does not exist."
+		exit 1
+	fi
+
+	if [[ ! -r "${filename}" ]]; then
+		print_error "Can't read file \"${input_file}\"."
+		exit 1
+	fi
+
+	while read -r line; do
+		command="$(echo $line | cut -d" " -f1)"
+		if [[ "$command" == "source" || "$command" == "." ]]; then
+			included_file=$(echo $line | cut -d" " -f 2)
+			expand_source "${included_file}"
+		else
+			echo "$line"
+		fi
+	done <"${filename}"
 }
